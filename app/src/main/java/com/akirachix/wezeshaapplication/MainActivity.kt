31a23c553b197220c0ab06de85587ad12deec3b1
Bridget-Kathure <bridgetkathuretitus@gmail.com>
2024-07-06 23:15:12 +0300

    added files

--- app/src/main/java/com/akirachix/wezeshaapplication/MainActivity.kt
+++ app/src/main/java/com/akirachix/wezeshaapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.akirachix.wezeshaapplication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,12 +8,18 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.akirachix.wezeshaapplication.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity<TextInputLayout> : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.tvLogin.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
     }
+
+
 }
